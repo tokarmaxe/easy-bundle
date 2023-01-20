32c690f57a2614b9540e5ad25624f9cb02126c4c
Maxim Tokar <tokarmaxe@gmail.com>
2023-01-20 20:18:46 +0200

    easy-bundle structure

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+namespace Maxim\EasyBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
