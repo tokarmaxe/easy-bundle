db3e4c0dbea707d11885822cc9d49014c42bc292
Maxim Tokar <tokarmaxe@gmail.com>
2023-01-20 20:35:09 +0200

    easy-bundle structure

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends AbstractController
 {
-    public function get(): Response
+    public function getAction(): Response
     {
         return $this->json([
             'success' => true,
